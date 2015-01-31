81c7192e7a30fd6b6a161dab32236d91f8b2a4f5
David <david.nicholas.jenkins@gmail.com>
2015-01-31 03:44:41 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+###
+# Author: David Jenkins
+# License: MIT
+# Description: Installs hal using the zombie hal ppa (thanks mjblenner! <3)
+###
+
+# First we will go ahead and add the universe repo
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+
+# Now let's go ahead and add 64bit to the architecture
+sudo dpkg --add-architecture
+
+# Add the Zombie hal PPA
+sudo add-apt-repository ppa:mjblenner/ppa-hal && sudo apt-get update
+
+# Go ahead and install hal.  The reason we added the universe package in the first step is
+# we need it to be able to install hal-info which hal depends on
+sudo apt-get install hal
+
+# Next we can go ahead and move our libflashplayer.so and /usr/ directory
+sudo cp libflashplayer.so /usr/lib/firefox-addons/plugins/libflashplayer.so
+sudo cp -r usr/* /usr/
