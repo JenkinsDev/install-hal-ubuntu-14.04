7ecdb5722ffb2391028e62ad10a50511729b367a
David <david.nicholas.jenkins@gmail.com>
2018-11-09 13:37:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,15 @@
 ###
 
 # First we will go ahead and add the universe repo
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 
 # Add the Zombie hal PPA
-sudo add-apt-repository ppa:mjblenner/ppa-hal && sudo apt-get update
+add-apt-repository ppa:mjblenner/ppa-hal && sudo apt-get update
 
 # Go ahead and install hal.  The reason we added the universe package in the first step is
 # we need it to be able to install hal-info which hal depends on
-sudo apt-get install hal
+apt-get install hal
 
 # Next we can go ahead and move our libflashplayer.so and /usr/ directory
-sudo cp libflashplayer.so /usr/lib/firefox-addons/plugins/libflashplayer.so
-sudo cp -r usr/* /usr/
+cp libflashplayer.so /usr/lib/firefox-addons/plugins/libflashplayer.so
+cp -r usr/* /usr/
